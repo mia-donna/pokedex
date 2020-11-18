52c081fc08a1324f605718b258ad741ae4b9cb36
Mia <mia@vidops.io>
2020-11-18 21:11:18 +0000

    added parse module with mapping for json data. Added just two data types and two fields for testing.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import HTTP
+import Parse
 
 main :: IO ()
 main = do
     let url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
     json <- download url
-    print json
+    case (parse json) of 
+        Left err -> print err
+        Right poke-> print . head $ (pokemon poke)
---
+++ src/Parse.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Parse where
+
+import Data.Aeson (eitherDecode)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import GHC.Generics ( Generic )
+import Data.Aeson.Types
+
+data Pokeinfo = Pokeinfo {
+    id :: Int,
+    name :: String
+} deriving (Show, Generic)
+
+instance FromJSON Pokeinfo
+instance ToJSON Pokeinfo
+
+data Pokemon = Pokemon {
+    pokemon :: [Pokeinfo]
+} deriving (Show, Generic)
+
+instance FromJSON Pokemon
+instance ToJSON Pokemon
+
+parse :: L8.ByteString -> Either String Pokemon
+parse json = eitherDecode json :: Either String Pokemon
